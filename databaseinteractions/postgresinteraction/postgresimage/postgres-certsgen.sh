6e9336d559187c58bd40ed247a50b2d74e6f825b
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-10-31 11:06:08 +0300

    feat: Написать код для разворачивания СУБД Postgres 	new file:   databaseinteractions/postgresinteraction/postgresimage/.env 	new file:   databaseinteractions/postgresinteraction/postgresimage/.gitignore 	new file:   databaseinteractions/postgresinteraction/postgresimage/README.md 	new file:   databaseinteractions/postgresinteraction/postgresimage/docker-compose.yml 	new file:   databaseinteractions/postgresinteraction/postgresimage/postgres-certsgen.sh

---
+++ databaseinteractions/postgresinteraction/postgresimage/postgres-certsgen.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+CERT_DIR="certs"
+
+echo "Удаляем из директории $CERT_DIR старые ключи и сертификаты..."
+if [ ! -d $CERT_DIR ]; then
+    mkdir $CERT_DIR
+else 
+    rm $CERT_DIR/*
+fi
+
+# ----- rootCA-key.pem -----
+echo "Генерация CA (корневого сертификата)..."
+openssl genrsa -out $CERT_DIR/rootCA-key.pem 2048
+openssl req -x509 -new -nodes -key $CERT_DIR/rootCA-key.pem -sha256 -days 3650 -out $CERT_DIR/rootCA.pem -subj "/C=RU/ST=Russia/L=Moscow/O=Org/CN=Local Root CA"
+
+echo "Установка пользователя Postgress (999) для корневого сертификата..."
+chown 999:999 $CERT_DIR/rootCA-key.pem
+
+echo "Генерация серверного ключа..."
+openssl genrsa -out $CERT_DIR/server-key.pem 2048
+openssl req -new -key $CERT_DIR/server-key.pem -out $CERT_DIR/server.csr -subj "/C=RU/ST=Russia/L=Moscow/O=Org/CN=localhost"
+
+echo "Установка пользователя Postgress (999) для серверного ключа..." 
+chown 999:999 $CERT_DIR/server-key.pem
+chmod 600 $CERT_DIR/server-key.pem
+
+cat > $CERT_DIR/server.ext << EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = localhost
+DNS.2 = postgres-server
+IP.1 = 127.0.0.1
+EOF
+
+echo "Генерация серверного ключа сертификата..."
+openssl x509 -req -in $CERT_DIR/server.csr -CA $CERT_DIR/rootCA.pem -CAkey $CERT_DIR/rootCA-key.pem -CAcreateserial -out $CERT_DIR/server-cert.pem -days 365 -sha256 -extfile $CERT_DIR/server.ext
+
+echo "Генерация клиентского ключа..."
+openssl genrsa -out $CERT_DIR/client-key.pem 2048
+openssl req -new -key $CERT_DIR/client-key.pem -out $CERT_DIR/client.csr -subj "/C=RU/ST=Russia/L=Moscow/O=Org/CN=Test Client"
+
+cat > $CERT_DIR/client.ext << EOF
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, keyEncipherment
+extendedKeyUsage = clientAuth
+EOF
+
+echo "Генерация клиентского сертификатов..."
+openssl x509 -req -in $CERT_DIR/client.csr -CA $CERT_DIR/rootCA.pem -CAkey $CERT_DIR/rootCA-key.pem -CAcreateserial -out $CERT_DIR/client-cert.pem -days 365 -sha256 -extfile $CERT_DIR/client.ext
+
+echo "Очистка..."
+rm $CERT_DIR/server.csr $CERT_DIR/server.ext $CERT_DIR/rootCA-key.pem $CERT_DIR/client.csr $CERT_DIR/client.ext
+
+echo "Сертификаты успешно сгенерированы!"
+echo "rootCA.pem - корневой сертификат"
+echo "server-key.pem - приватный ключ сервера"
+echo "server-cert.pem - сертификат сервера"
+echo "client-key.pem - ключ клиента"
+echo "client-cert.pem - сертификат клиента"
