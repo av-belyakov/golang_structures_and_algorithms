90b6fea31bd487214118129470567e2fd16c1b86
Artemiy <beljakovav@gmail.com>
2026-03-02 15:13:57 +0300

    test: Написать дополнительные тесты методов Clickhouse 	modified:   databaseinteractions/clickhousedb/clickhousegopackage_test.go 	modified:   go.mod 	modified:   go.sum

--- databaseinteractions/clickhousedb/clickhousegopackage_test.go
+++ databaseinteractions/clickhousedb/clickhousegopackage_test.go
@@ -1,29 +1,27 @@
 package clickhousedb_test
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"testing"
+	"time"
 
-	"github.com/av-belyakov/golang_structures_and_algorithms/databaseinteractions/clickhousedb"
 	"github.com/brianvoe/gofakeit/v7"
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/av-belyakov/golang_structures_and_algorithms/databaseinteractions/clickhousedb"
 )
 
-// для подробной информации https://clickhouse.com/docs/integrations/go
+// для подробной информации https://clickhouse.com/docs/ru/integrations/go
 
 func TestClickhouseGoPackage(t *testing.T) {
 	if err := godotenv.Load("./clickhouseimage/.env"); err != nil {
 		log.Fatal(err)
 	}
 
-	/*
-		Database: os.Getenv("CLICKHOUSE_SERVER_DB"),
-		Username: os.Getenv("CLICKHOUSE_SERVER_USER"),
-		Password: os.Getenv("CLICKHOUSE_SERVER_PASSWORD"),
-	*/
 	port, err := strconv.ParseInt(os.Getenv("CLICKHOUSE_SERVER_YOUR_OWN_PORT"), 10, 32)
 	if err != nil {
 		log.Fatal(err)
@@ -62,25 +60,29 @@ func TestClickhouseGoPackage(t *testing.T) {
 					object_name String,
 					country String,
 					address String,
+					email String,
 					geographical_coordinate_latitude Float64,
 					geographical_coordinate_longitude Float64,
 					date_of_construction Date,
 					date_of_commissioning Date,
-					total_area Float64,
-					living_area Float64,
+					total_area INT,
+					living_area INT,
 					price Float64
-					)`,
+					)
+				ENGINE = MergeTree()
+				PRIMARY KEY (object_name, country)`,
 			))
 		})
 
 		t.Run("Тест 2.2. Заполняем тестовую таблицу данными", func(t *testing.T) {
-			for range 350 {
+			for range 1_000 {
 				assert.NoError(t, conn.Exec(t.Context(),
 					`INSERT INTO real_estate_objects_example(
 					uuid,
 					object_name,
 					country,
 					address,
+					email,
 					geographical_coordinate_latitude,
 					geographical_coordinate_longitude,
 					date_of_construction,
@@ -88,25 +90,112 @@ func TestClickhouseGoPackage(t *testing.T) {
 					total_area,
 					living_area,
 					price
-				) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`,
+				) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`,
 					gofakeit.UUID(),
 					gofakeit.Name(),
 					gofakeit.Country(),
 					gofakeit.Address().Address,
+					gofakeit.Email(),
 					gofakeit.Latitude(),
 					gofakeit.Longitude(),
 					gofakeit.Date(),
 					gofakeit.Date(),
-					gofakeit.Float64(),
-					gofakeit.Float64(),
+					gofakeit.IntRange(50, 3_000),
+					gofakeit.IntRange(30, 2_500),
 					gofakeit.Price(1_000_000, 1_000_000_000),
 				))
 			}
 		})
 	})
 
-	//t.Run("Тест 3. Удаление тестовой таблицы", func(t *testing.T) {
-	//	conn.Exec(context.Background(), `DROP TABLE IF EXISTS real_estate_objects_example`)
+	t.Run("Тест 3. Выполнить запросы к БД", func(t *testing.T) {
+		t.Run("Тест 3.1. Поиск по стране", func(t *testing.T) {
+			rows, err := conn.Query(
+				t.Context(),
+				`SELECT uuid, country, address, email, date_of_construction, price
+				FROM real_estate_objects_example 
+				WHERE country IN ('Russia', 'Franc', 'USA', 'China', 'Spain', 'Niger');`,
+			)
+			assert.NoError(t, err)
+
+			num := 1
+			for rows.Next() {
+				var (
+					uuid                 string
+					country              string
+					address              string
+					email                string
+					date_of_construction time.Time
+					price                float64
+				)
+
+				assert.NoError(t, rows.Scan(&uuid, &country, &address, &email, &date_of_construction, &price))
+
+				fmt.Printf(
+					"%d. Found country:'%s', uuid:'%s', address:'%s', email:'%s', date_of_construction:'%v', price:'%v'\n",
+					num, country, uuid, address, email, date_of_construction, price,
+				)
+
+				num++
+			}
+
+			rows.Close()
+		})
+
+		t.Run("Тест 3.2. Обновление данных", func(t *testing.T) {
+			construction := "2002-02-17"
+			commissioning := "2021-01-01"
+
+			assert.NoError(t, conn.Exec(
+				t.Context(),
+				`ALTER TABLE clickhouse_database_example.real_estate_objects_example
+				UPDATE date_of_construction = '2002-02-17', date_of_commissioning = '2021-01-01'
+				WHERE date_of_construction = '1970-01-01' OR date_of_commissioning = '1970-01-01';`,
+			))
+
+			rows, err := conn.Query(
+				t.Context(),
+				`SELECT uuid, object_name 
+				FROM real_estate_objects_example 
+				WHERE date_of_construction = $1 AND date_of_commissioning = $2;`,
+				construction,
+				commissioning,
+			)
+			assert.NoError(t, err)
+
+			num := 1
+			fmt.Println("Found next items:")
+			for rows.Next() {
+				var uuid, objectName string
+				assert.NoError(t, rows.Scan(&uuid, &objectName))
+
+				fmt.Printf("%d. %s %s\n", num, uuid, objectName)
+
+				num++
+			}
+
+			rows.Close()
+		})
+	})
+
+	t.Run("Тест 4. Генерация случайных данных средствами Clickhouse", func(t *testing.T) {
+		assert.NoError(t, conn.Exec(
+			t.Context(),
+			`CREATE TABLE IF NOT EXISTS my_test_table_example
+			ENGINE = MergeTree
+			ORDER BY tuple()
+			AS SELECT * 
+			FROM generateRandom(
+				'col1 UInt32, col2 String, col3 Float64, col4 DateTime',
+				1,  -- seed for data generation
+				10  -- number of different random values
+			)
+			LIMIT 100;`,
+		))
+	})
+
+	//t.Run("Тест 5. Удаление тестовой таблицы", func(t *testing.T) {
+	//	conn.Exec(t.Context(), `DROP TABLE IF EXISTS real_estate_objects_example`)
 	//})
 
 	t.Cleanup(func() {
--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module github.com/av-belyakov/golang_structures_and_algorithms
 go 1.26
 
 require (
+	github.com/ClickHouse/clickhouse-go/v2 v2.43.0
+	github.com/brianvoe/gofakeit/v7 v7.14.0
 	github.com/confluentinc/confluent-kafka-go/v2 v2.11.1
+	github.com/elastic/go-elasticsearch/v9 v9.3.0
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/google/uuid v1.6.0
 	github.com/jedib0t/go-pretty/v6 v6.6.1
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.11.2
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/minio/minio-go/v7 v7.0.95
+	github.com/nats-io/nats.go v1.46.0
 	github.com/redis/go-redis/v9 v9.7.1
 	github.com/segmentio/kafka-go v0.4.49
 	github.com/stretchr/testify v1.11.1
@@ -19,31 +24,24 @@ require (
 
 require (
 	github.com/ClickHouse/ch-go v0.71.0 // indirect
-	github.com/ClickHouse/clickhouse-go v1.5.4 // indirect
-	github.com/ClickHouse/clickhouse-go/v2 v2.43.0 // indirect
 	github.com/andybalholm/brotli v1.2.0 // indirect
-	github.com/brianvoe/gofakeit/v7 v7.14.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/cloudflare/golz4 v0.0.0-20150217214814-ef862a3cdc58 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/elastic/elastic-transport-go/v8 v8.8.0 // indirect
-	github.com/elastic/go-elasticsearch/v9 v9.3.0 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
-	github.com/goforj/godump v1.9.0 // indirect
 	github.com/klauspost/compress v1.18.3 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
-	github.com/lib/pq v1.11.2 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/minio/crc64nvme v1.0.2 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
-	github.com/nats-io/nats.go v1.46.0 // indirect
+	github.com/moby/sys/atomicwriter v0.1.0 // indirect
 	github.com/nats-io/nkeys v0.4.11 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/paulmach/orb v0.12.0 // indirect
